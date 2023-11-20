2ed8cc54201b789d89e6c95212fc3cf5a6ca1e6f
shaon07softic <shamirul@softic.ai>
2023-11-20 14:34:26 +0600

    [add] custom image

--- app/page.tsx
+++ app/page.tsx
@@ -6,12 +6,13 @@ export default function Home() {
       <h1 className="text-3xl font-bold underline">Hello World</h1>
       <Image
         src={
-          "https://images.unsplash.com/photo-1699115174617-7e49afc54da5?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+          "https://images.unsplash.com/photo-1699730132083-360f1d7a8f83?q=80&w=1936&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
         }
         width={500}
         height={500}
         alt="hello"
       />
+      <Image src={`/dd.jpg`} width={500} height={500} alt="hello" />
     </main>
   );
 }
